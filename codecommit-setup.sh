e9bf527ad1adc5628b8dabd63e530f87fbf5ba86
Mike <sw1277@gmail.com>
2020-08-01 02:17:54 -0400

    codecommit instructions notes

---
+++ codecommit-setup.sh
@@ -0,0 +1,9 @@
+# codecommit https setup instructions, and a commit
+# https://docs.aws.amazon.com/codecommit/latest/userguide/setting-up-gc.html
+
+cd ~/code/
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/your-repo-name 
+cd your-repo-name 
+git add -A 
+git commit -m 'your comment'
+git push
