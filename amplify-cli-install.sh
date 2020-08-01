bfb67d021dd952bc9f8c85810225ac698c655dff
Mike <sw1277@gmail.com>
2020-08-01 02:09:50 -0400

    added amplify

---
+++ amplify-cli-install.sh
@@ -0,0 +1,6 @@
+# amplify CLI config instructions:
+# https://docs.amplify.aws/start/getting-started/installation/q/integration/js
+# Mac 10.15
+sudo npm install npm
+sudo npm install -g @aws-amplify/cli
+amplify configure
